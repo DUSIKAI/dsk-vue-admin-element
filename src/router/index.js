fdb8d432c3cb3b23fc5725538529a2eddebd8a81
DUSIKAI <935233332@qq.com>
2023-03-25 13:02:39 +0800

    默认路由修改

--- src/router/index.js
+++ src/router/index.js
@@ -7,10 +7,10 @@ Vue.use(Router)
 import Layout from '@/layout'
 
 /* Router Modules */
-import componentsRouter from './modules/components'
-import chartsRouter from './modules/charts'
-import tableRouter from './modules/table'
-import nestedRouter from './modules/nested'
+// import componentsRouter from './modules/components'
+// import chartsRouter from './modules/charts'
+// import tableRouter from './modules/table'
+// import nestedRouter from './modules/nested'
 
 /**
  * Note: sub-menu only appear when route children.length >= 1
@@ -73,7 +73,7 @@ export const constantRoutes = [
   {
     path: '/',
     component: Layout,
-    redirect: '/table/complex-table',
+    redirect: '/complex-table',
     name: 'Table',
     meta: {
       title: 'Table',
